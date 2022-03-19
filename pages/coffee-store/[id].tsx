ecc2d13c0e96f028048973ab7fbdfe8458a91509
Marc <91346941+md1440@users.noreply.github.com>
2022-03-19 21:20:04 +0100

    add getStaticProps getStaticPaths

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -1,20 +1,71 @@
+/* eslint-disable react/no-unused-prop-types */
+/* eslint-disable arrow-body-style */
+import { GetStaticPathsResult, GetStaticPropsResult } from "next";
+import { Params } from "next/dist/server/router";
+import Head from 'next/head';
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { ReactElement } from "react";
+import coffeeStoresData from "../../data/coffee-stores.json";
 
-function CoffeeStore(): ReactElement {
+interface CoffeeStores {
+	coffeeStores: CoffeeStore[];
+}
+
+interface CoffeeStore {
+	id: number;
+	name: string;
+	imgUrl: string;
+	websiteUrl: string;
+	address: string;
+	neighbourhood: string;
+}
+
+export async function getStaticProps({ params }: Params) {
+	return {
+		props: {
+			coffeeStore: coffeeStoresData.find((coffeeStore: CoffeeStore) => {
+				return coffeeStore.id.toString() === params.id;
+			}),
+		}, // will be passed to the page component as props
+	};
+}
+
+export async function getStaticPaths(): Promise<GetStaticPathsResult<Params>> {
+	const paths = coffeeStoresData.map((coffeeStore: CoffeeStore) => {
+		return { params: { id: coffeeStore.id.toString()}}
+	})
+
+	return {
+		paths,
+		fallback: true,
+	};
+}
+
+interface Props {
+	coffeeStore: CoffeeStore;
+}
+
+function CoffeeStorePage({coffeeStore}: Props): ReactElement {
 	const router = useRouter();
+
+	if(router.isFallback) {
+		return <div>Loading...</div>
+	}
+
 	return (
 		<div>
-			CoffeeStore {router.query.id}
+			<Head>
+				<title>{coffeeStore.name}</title>
+			</Head>
 			<Link href="/">
 				<a>Back to Home</a>
 			</Link>
-			<Link href="/coffee-store/dynamic">
-				<a>Got page dynamic</a>
-			</Link>
+			<p>{coffeeStore.name}</p>
+			<p>{coffeeStore.address}</p>
+			<p>{coffeeStore.neighbourhood}</p>
 		</div>
 	);
 }
 
-export default CoffeeStore;
+export default CoffeeStorePage;

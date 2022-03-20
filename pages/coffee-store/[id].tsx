1e10c3d26b6999830c0c43abd3dacd7122027469
Marc <91346941+md1440@users.noreply.github.com>
2022-03-20 19:27:59 +0100

    add [id] coffee-store page functionality and styling

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/no-unused-prop-types */
 /* eslint-disable arrow-body-style */
-import { GetStaticPathsResult, GetStaticPropsResult } from "next";
+import cls from "classnames";
+import { GetStaticPathsResult } from "next";
 import { Params } from "next/dist/server/router";
-import Head from 'next/head';
+import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { ReactElement } from "react";
 import coffeeStoresData from "../../data/coffee-stores.json";
+import styles from "../../styles/CoffeeStore.module.css";
 
 interface CoffeeStores {
 	coffeeStores: CoffeeStore[];
@@ -33,8 +36,8 @@ export async function getStaticProps({ params }: Params) {
 
 export async function getStaticPaths(): Promise<GetStaticPathsResult<Params>> {
 	const paths = coffeeStoresData.map((coffeeStore: CoffeeStore) => {
-		return { params: { id: coffeeStore.id.toString()}}
-	})
+		return { params: { id: coffeeStore.id.toString() } };
+	});
 
 	return {
 		paths,
@@ -46,24 +49,50 @@ interface Props {
 	coffeeStore: CoffeeStore;
 }
 
-function CoffeeStorePage({coffeeStore}: Props): ReactElement {
+function CoffeeStorePage({ coffeeStore }: Props): ReactElement {
 	const router = useRouter();
 
-	if(router.isFallback) {
-		return <div>Loading...</div>
+	const handleUpvoteButton = () => {};
+
+	if (router.isFallback) {
+		return <div>Loading...</div>;
 	}
 
 	return (
-		<div>
+		<div className={styles.layout}>
 			<Head>
 				<title>{coffeeStore.name}</title>
 			</Head>
-			<Link href="/">
-				<a>Back to Home</a>
-			</Link>
-			<p>{coffeeStore.name}</p>
-			<p>{coffeeStore.address}</p>
-			<p>{coffeeStore.neighbourhood}</p>
+			<div className={styles.container}>
+				<div className={styles.col1}>
+					<div className={styles.backToHomeLink}>
+						<Link href="/">
+							<a>Back to Home</a>
+						</Link>
+					</div>
+					<div className={styles.nameWrapper}>
+						<h1 className={styles.name}>{coffeeStore.name}</h1>
+					</div>
+					<Image src={coffeeStore.imgUrl} width={600} height={360} className={styles.storeImg} alt={coffeeStore.name} />
+				</div>
+				<div className={cls("glass", styles.col2)}>
+					<div className={styles.iconWrapper}>
+						<Image src="/static/icons/places.svg" width="24" height="24" />
+						<p className={styles.text}>{coffeeStore.address}</p>
+					</div>
+					<div className={styles.iconWrapper}>
+						<Image src="/static/icons/nearMe.svg" width="24" height="24" />
+						<p className={styles.text}>{coffeeStore.neighbourhood}</p>
+					</div>
+					<div className={styles.iconWrapper}>
+						<Image src="/static/icons/star.svg" width="24" height="24" />
+						<p className={styles.text}>{1}</p>
+					</div>
+					<button type="button" className={styles.upvoteButton} onClick={handleUpvoteButton}>
+						Like
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
